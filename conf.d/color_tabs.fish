327267056bb535b1384fc93c188b464cbaf23f77
Albin Ekblom <albinekblom@gmail.com>
2018-05-14 12:58:45 +0200

    🎉 Add plugin

---
+++ conf.d/color_tabs.fish
@@ -0,0 +1,21 @@
+function tab_color -d "Set tab color"
+    echo -ne "\033]6;1;bg;red;brightness;$argv[1]\a"
+    echo -ne "\033]6;1;bg;green;brightness;$argv[2]\a"
+    echo -ne "\033]6;1;bg;blue;brightness;$argv[3]\a"
+end
+
+function reset_tab_color -d "Reset tab color"
+    echo -ne "\033]6;1;bg;*;default\a"
+end
+
+function chpwd --on-variable PWD
+  if string match '*/frontend-*' $PWD >> /dev/null
+    tab_color 104 159 56
+  else if string match '*/service-*' $PWD >> /dev/null
+    tab_color 249 168 37
+  else if string match '*/app-*' $PWD >> /dev/null
+    tab_color 104 159 56
+  else
+    reset_tab_color
+  end
+end
